f63cb7fbe59f84b0341e5859b434f34622f35399
Kari Lovise Ness <kariln@stud.ntnu.no>
2020-03-01 17:54:44 +0100

    Problem set 3

--- Problem set 3/problem_set3_1a.m
+++ Problem set 3/problem_set3_1a.m
@@ -26,9 +26,9 @@ for y = 1:1:length(dp)
     grid on;
     for x = 1:1:length(m)
         m_temp = m(x);
-        for n = 1:1:N
-            R_temp = R(p(n));
-            sigma_vec(x,n) = sigma(m_temp,dp_temp,R_temp);
+        for z = 1:1:N
+            R_temp = R(p(z));
+            sigma_vec(x,z) = sigma(m_temp,dp_temp,R_temp);
         end
         txt = ['m = ',num2str(m_temp)];
         plot(p,sigma_vec(x,:),'DisplayName',txt);
@@ -42,4 +42,35 @@ for y = 1:1:length(dp)
     end
 end
 
-    
+%%
+%task 1d
+%plotting p(t) with several values of m
+
+N = 1000;
+t = linspace(0,100,N);
+p = zeros(length(m),N); 
+load = 200; %[Mpa]
+
+%creating functions
+p_func = @(t,m)... %the equivalent plastic strain as a function of t
+    ((m+n)/m*dp0*(load/K)^(1/m)*t)^(m/(n+m));
+
+%calculating the values with the function
+figure();
+hold on;
+grid on;
+xlabel('Time, t [s]');
+ylabel('Equivalent plastic strain, p')
+title('Equivalent plastic strain as a function of time');
+for i = 1:1:length(m)
+    m_tmp = m(i);
+    for j = 1:1:N
+        p(i,j) = p_func(t(j),m_tmp); 
+    end
+    text = ['m = ',num2str(m_tmp)];
+    plot(t,p(i,:),'DisplayName',text);
+    hold on;
+end
+legend show;
+saveas(gcf,'eq_pl_str.png');
+hold off;
